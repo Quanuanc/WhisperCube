3e829bb0e3f76ec330e5c7497374449e3244ca76
Quanuanc <quanuanc@outlook.com>
2024-02-26 22:54:29 +0800

    Initial Commit

---
+++ WhisperCube/WhisperCubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperCubeApp.swift
+//  WhisperCube
+//
+//  Created by cheng on 2024/2/26.
+//
+
+import SwiftUI
+
+@main
+struct WhisperCubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
