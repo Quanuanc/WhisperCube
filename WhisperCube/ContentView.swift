775a550cf9ee3b37c8c10e4638b030a20270b83b
Quanuanc <quanuanc@outlook.com>
2024-02-26 23:17:11 +0800

    Add main ContentView

--- WhisperCube/ContentView.swift
+++ WhisperCube/ContentView.swift
@@ -8,17 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var modelLoaded = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if modelLoaded {
+                toggleRecordButton
+            } else {
+                loadModelButton
+            }
+
+            ScrollView {
+                Text(verbatim: "Hello, this is a sentence")
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/ .infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+            }
         }
         .padding()
     }
 }
 
+extension ContentView {
+    var toggleRecordButton: some View {
+        Button {
+            print("start")
+        } label: {
+            Text("Start recording")
+        }
+        .buttonStyle(.bordered)
+        .disabled(true)
+    }
+
+    var loadModelButton: some View {
+        Button {
+            modelLoaded = true
+        } label: {
+            Text("Load model")
+        }
+    }
+}
+
 #Preview {
     ContentView()
+        .frame(width: 300, height: 200)
 }
--- WhisperCube/WhisperCubeApp.swift
+++ WhisperCube/WhisperCubeApp.swift
@@ -12,6 +12,7 @@ struct WhisperCubeApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .frame(minWidth: 300, minHeight: 200)
         }
     }
 }
